02bb9876e3ad8021418889e8c68b02a3bf4b62d4
franromero0 <francisconahuel09@gmail.com>
2024-05-01 22:40:50 -0300

    web finalizada 1.2

--- js/main.js
+++ js/main.js
@@ -1,48 +1,48 @@
 // Detecta cuando la página esta totalmente cargada y pone display none en el loader de width y heighto 100%.
 document.addEventListener('DOMContentLoaded', function() {
-  window.addEventListener('load', function() {
-      document.querySelector('.loader-container').style.display = 'none';
-  });
+    window.addEventListener('load', function() {
+        document.querySelector('.loader-container').style.display = 'none';
+    });
 });
 // Boton flotante aparece a los 100px de scroll.
 window.addEventListener('scroll', function() {
     var botonFlotante = document.getElementById('boton-flotante');
     if (window.scrollY > 100) { 
-      botonFlotante.style.display = 'block'; 
-    } else {
-      botonFlotante.style.display = 'none'; 
-    }
-  });
-// Toggle de background color para el header cuando scrolleo.
-document.addEventListener('DOMContentLoaded', function() {
-  var menuLinks = document.querySelectorAll('.ul-links a');
-  var menuCheckbox = document.getElementById('menu_hamburguesa');
+        botonFlotante.style.display = 'block'; 
+        } else {
+        botonFlotante.style.display = 'none'; 
+        }
+    });
+    // Toggle de background color para el header cuando scrolleo.
+    document.addEventListener('DOMContentLoaded', function() {
+    var menuLinks = document.querySelectorAll('.ul-links a');
+    var menuCheckbox = document.getElementById('menu_hamburguesa');
 
-  menuLinks.forEach(function(link) {
-      link.addEventListener('click', function() {
-          menuCheckbox.checked = false;
-      });
-  });
-});
-window.onscroll = function() {
-  if (window.scrollY === 0) {
-    document.getElementById("header").classList.add("transparent");
-    document.getElementById("header").classList.remove("solid");
-  } else {
-    document.getElementById("header").classList.add("solid");
-    document.getElementById("header").classList.remove("transparent");
-  }
+    menuLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            menuCheckbox.checked = false;
+        });
+    });
+    });
+    window.onscroll = function() {
+    if (window.scrollY === 0) {
+        document.getElementById("header").classList.add("transparent");
+        document.getElementById("header").classList.remove("solid");
+    } else {
+        document.getElementById("header").classList.add("solid");
+        document.getElementById("header").classList.remove("transparent");
+    }
 }
 
 let prevScrollPos = window.pageYOffset;
 window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollPos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.top = "-40px"; 
-  }
-  prevScrollPos = currentScrollPos;
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-40px"; 
+    }
+    prevScrollPos = currentScrollPos;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -57,14 +57,14 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 window.addEventListener('scroll', function() {
-  var header = document.querySelector('header');
-  var scrollPosition = window.scrollY;
+    var header = document.querySelector('header');
+    var scrollPosition = window.scrollY;
 
-  if (scrollPosition > 1) { 
-      header.style.backgroundImage = "linear-gradient(to top, #9a8884, #9a8884, #9a8884, #9a8884, #9a8884, #94827e, #8e7c77, #887671, #7b6963, #6f5b56)";
-  } else {
-      header.style.backgroundImage = "none";
-  }
+    if (scrollPosition > 1) { 
+        header.style.backgroundImage = "linear-gradient(to top, #9a8884, #9a8884, #9a8884, #9a8884, #9a8884, #94827e, #8e7c77, #887671, #7b6963, #6f5b56)";
+    } else {
+        header.style.backgroundImage = "none";
+    }
 });
 // Funcion para cuando el usuario toque el menú no pueda scrollear.
 document.addEventListener('DOMContentLoaded', function() {
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     document.body.classList.remove('hidden'); // Quita hidden
                 }
             });
-            header.style.backgroundImage = originalBackground; // Restaura el fondo original
+            header.style.backgroundImage = "linear-gradient(to top, #9a8884, #9a8884, #9a8884, #9a8884, #9a8884, #94827e, #8e7c77, #887671, #7b6963, #6f5b56)";// Restaura el fondo original
         }
     });
 
@@ -104,42 +104,6 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
-
-
-// // Funcion para ver si está en safari 9 donde el fondo del menú mobile está con backdrop-filter y no es compatible.
-// document.addEventListener('DOMContentLoaded', function() {
-//   const menuCheckbox = document.querySelector('.menu-hamburguesa');
-//   const header = document.getElementById('header');
-
-//   menuCheckbox.addEventListener('change', function() {
-//       if (menuCheckbox.checked) {
-//           header.style.backgroundImage = "linear-gradient(to top, #9a8884, #9a8884, #9a8884, #9a8884, #9a8884, #94827e, #8e7c77, #887671, #7b6963, #6f5b56)";
-//           setHeaderBackground(); 
-//           document.body.classList.add('hidden'); 
-//       } else {
-//           header.addEventListener('transitionend', function(event) {
-//               if (event.propertyName === 'background-image') {
-//                   document.body.classList.remove('hidden'); // Quita la clase hidden
-//               }
-//           });
-//           header.style.backgroundImage = "none"; // Restablece el fondo a su estado original
-//       }
-//   });
-
-//   function setHeaderBackground() {
-//       // Verifica si el navegador admite backdrop-filter
-//       if (!supportsBackdropFilter()) {
-//           header.style.backgroundColor = "rgba(255, 255, 255, 0.5)"; // Cambiar el color de fondo si backdrop-filter no es compatible
-//       }
-//   }
-
-//   // Función para verificar si el navegador admite backdrop-filter
-//   function supportsBackdropFilter() {
-//       const testElem = document.createElement('div');
-//       testElem.style.backdropFilter = 'blur(10px)';
-//       return testElem.style.backdropFilter === 'blur(10px)';
-//   }
-// });
 
 // Slider Home
 let sliderInner = document.querySelector(".slider--inner");
